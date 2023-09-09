ffa91c84bb249644b4095950835b2ab3bbca234b
dada-tech <david.olu.dada@gmail.com>
2023-09-09 14:31:20 -0600

    toast with email

--- app/src/main/java/edu/northeastern/numad23fa_daviddada/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa_daviddada/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.northeastern.numad23fa_daviddada;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button aboutMeButton = findViewById(R.id.button_about_me);
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "David Dada\ndada.d@northeastern.edu", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
