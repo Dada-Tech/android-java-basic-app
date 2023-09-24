08a9a7c8f65a948404cd6674d84091ffc8b6915e
dada-tech <david.olu.dada@gmail.com>
2023-09-23 21:23:50 -0600

    dynamically set TextView on button click

--- app/src/main/java/edu/northeastern/numad23fa_daviddada/ButtonGridExampleActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa_daviddada/ButtonGridExampleActivity.java
@@ -2,19 +2,46 @@ package edu.northeastern.numad23fa_daviddada;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ButtonGridExampleActivity extends AppCompatActivity {
+    String defaultPressedText = "-";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_grid_buttons);
 
-        final Button aboutMeButton = findViewById(R.id.button_about_me);
-        aboutMeButton.setOnClickListener(v -> Toast.makeText(v.getContext(), "David Dada\ndada.d@northeastern.edu", Toast.LENGTH_SHORT).show());
+        // default pressed text
+        setPressedText(defaultPressedText);
+
+        // button logic
+        final Button clickyButtonA = findViewById(R.id.clicky_A);
+        clickyButtonA.setOnClickListener(v -> setPressedText("A"));
+
+        final Button clickyButtonB = findViewById(R.id.clicky_B);
+        clickyButtonB.setOnClickListener(v -> setPressedText("B"));
+
+        final Button clickyButtonC = findViewById(R.id.clicky_C);
+        clickyButtonC.setOnClickListener(v -> setPressedText("C"));
+
+        final Button clickyButtonD = findViewById(R.id.clicky_D);
+        clickyButtonD.setOnClickListener(v -> setPressedText("D"));
+
+        final Button clickyButtonE = findViewById(R.id.clicky_E);
+        clickyButtonE.setOnClickListener(v -> setPressedText("E"));
+
+        final Button clickyButtonF = findViewById(R.id.clicky_F);
+        clickyButtonF.setOnClickListener(v -> setPressedText("F"));
+    }
+
+    // Set text of Pressed Text View
+    void setPressedText(String text) {
+        String newTextValue = getText(R.string.pressed) + " " + text + " ";
+        final TextView pressedTextView = (TextView) findViewById(R.id.pressed_text);
+        pressedTextView.setText(newTextValue);
     }
 
 }
