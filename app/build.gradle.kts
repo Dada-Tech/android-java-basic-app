12100cdbee42ac1286235c339d330ac9695ac0b0
dada-tech <david.olu.dada@gmail.com>
2023-10-08 01:20:56 -0600

    prime threading updating the ui in non-blocking way

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,9 @@ android {
 
 dependencies {
 
+    implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/edu/northeastern/numad23fa_daviddada/PrimeDirectiveActivity.java
+++ app/src/main/java/edu/northeastern/numad23fa_daviddada/PrimeDirectiveActivity.java
@@ -1,14 +1,127 @@
 package edu.northeastern.numad23fa_daviddada;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.apache.commons.math3.primes.Primes;
+
 public class PrimeDirectiveActivity extends AppCompatActivity {
+    TextView currentNumberTextView;
+    TextView lastPrimeTextView;
+
+    Thread primeThread;
+
+    Handler primeHandler;
+
+    String logTag = "--------- PrimeDirective ---------";
+
+    boolean isThreadRunning;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime_directive);
+
+        primeHandler = new Handler(Looper.getMainLooper());
+
+        // Create and start a background thread
+        primeThread = new Thread(new PrimeRunner(primeHandler));
+
+        // current number text view
+        currentNumberTextView = findViewById(R.id.prime_dir_current_number_text_view);
+        currentNumberTextView.setText(getString(R.string.current_number_prime_text, " "));
+
+        // last prime text view
+        lastPrimeTextView = findViewById(R.id.prime_dir_last_prime_text_view);
+        lastPrimeTextView.setText(getString(R.string.last_prime_text, " "));
+
+        // find primes button
+        final Button findPrimesButton = findViewById(R.id.prime_dir_find_primes_button);
+        findPrimesButton.setOnClickListener(v -> {
+            if (!isThreadRunning) {
+                primeThread = new Thread(new PrimeRunner(primeHandler));
+                primeThread.start();
+                isThreadRunning = true;
+            } else {
+                Log.d(logTag, "ALREADY RUNNING!");
+            }
+        });
+
+        // terminate search button
+        final Button terminateSearchButton = findViewById(R.id.prime_dir_terminate_search_button);
+        terminateSearchButton.setOnClickListener(v -> {
+            if (isThreadRunning) {
+                primeThread.interrupt();
+                isThreadRunning = false;
+            } else {
+                Log.d(logTag, "tried to interrupt on dead thread");
+            }
+        });
+
+        // pacifier switch checkbox
+        final CheckBox pacifierSwitchCheckbox = findViewById(R.id.prime_dir_pacifier_switch_checkbox);
+        pacifierSwitchCheckbox.setOnClickListener(v -> pacifierSwitchCheckbox.setActivated(!pacifierSwitchCheckbox.isActivated()));
     }
 
+    class PrimeRunner implements Runnable {
+        Handler handler;
+
+        private final int n_skips = 20000;
+
+        private int currentNumber;
+        private int lastPrimeNumber;
+
+        PrimeRunner(Handler handler) {
+            this.handler = handler;
+            currentNumber = 3;
+        }
+
+        private boolean isPrime(int number) {
+            return Primes.isPrime(number);
+        }
+
+        @Override
+        public void run() {
+            int skipCount = 0;
+
+            while (!Thread.currentThread().isInterrupted()) {
+                if (++skipCount % n_skips == 0) {
+                    skipCount = 0;
+                    currentNumber += 2;
+
+                    // if prime, update
+                    if (isPrime(currentNumber)) {
+                        lastPrimeNumber = currentNumber;
+
+                        // last prime post in main handler
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                lastPrimeTextView.setText(getString(R.string.last_prime_text, "" + lastPrimeNumber));
+                            }
+                        });
+                    }
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            currentNumberTextView.setText(getString(R.string.current_number_prime_text, "" + currentNumber));
+                        }
+                    });
+
+                    if (Thread.currentThread().isInterrupted()) {
+                        Log.d(logTag, "Is Interrupted");
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
