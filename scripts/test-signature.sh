9fd98d8e4ac625c6d8f7d66ff41c400dd2e4ed52
dada-tech <david.olu.dada@gmail.com>
2023-10-08 18:24:08 -0600

    enhanced script to show true

--- scripts/test-signature.sh
+++ scripts/test-signature.sh
@@ -1,1 +1,1 @@
-/Users/daviddada/Library/Android/sdk/build-tools/34.0.0/apksigner verify --verbose ./app/build/outputs/apk/release/NUMAD23Fa_DavidDada.apk
+/Users/daviddada/Library/Android/sdk/build-tools/34.0.0/apksigner verify --verbose ./app/build/outputs/apk/release/NUMAD23Fa_DavidDada.apk | grep true
